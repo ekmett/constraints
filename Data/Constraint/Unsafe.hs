b406846b97320f6e9dc81b3ae6ef00c2ea9fbe3d
Edward Kmett <ekmett@gmail.com>
2011-11-11 12:49:18 -0500

    added unsafe newtype deriving and underiving

--- Data/Constraint/Unsafe.hs
+++ Data/Constraint/Unsafe.hs
@@ -10,6 +10,8 @@
 
 module Data.Constraint.Unsafe
   ( evil
+  , derive
+  , underive
   -- * Sugar
   , applicative
   , alternative
@@ -17,15 +19,21 @@ module Data.Constraint.Unsafe
 
 import Control.Applicative
 import Control.Monad
+import Control.Newtype
 import Data.Constraint
 import Unsafe.Coerce
 
 evil :: a :- b
 evil = unsafeCoerce refl
 
+derive :: Newtype n o => (o -> n) -> t o :- t n
+derive _ = evil
+
+underive :: Newtype n o => (o -> n) -> t n :- t o
+underive _ = evil
+
 applicative :: forall m a. Monad m => (Applicative m => m a) -> m a
 applicative m = m \\ trans (evil :: Applicative (WrappedMonad m) :- Applicative m) ins
 
 alternative :: forall m a. MonadPlus m => (Alternative m => m a) -> m a
 alternative m = m \\ trans (evil :: Alternative (WrappedMonad m) :- Alternative m) ins
-
