6037de1dea08bd3db04eb95ed368ad2a5d48ab06
Edward Kmett <ekmett@gmail.com>
2013-10-28 00:52:47 -0400

    copyright update

--- Data/Constraint.hs
+++ Data/Constraint.hs
@@ -19,7 +19,7 @@
 -----------------------------------------------------------------------------
 -- |
 -- Module      :  Data.Constraint
--- Copyright   :  (C) 2011-2012 Edward Kmett,
+-- Copyright   :  (C) 2011-2013 Edward Kmett,
 -- License     :  BSD-style (see the file LICENSE)
 --
 -- Maintainer  :  Edward Kmett <ekmett@gmail.com>
--- Data/Constraint/Unsafe.hs
+++ Data/Constraint/Unsafe.hs
@@ -11,7 +11,7 @@
 -----------------------------------------------------------------------------
 -- |
 -- Module      :  Data.Constraint.Unsafe
--- Copyright   :  (C) 2011-2012 Edward Kmett
+-- Copyright   :  (C) 2011-2013 Edward Kmett
 -- License     :  BSD-style (see the file LICENSE)
 --
 -- Maintainer  :  Edward Kmett <ekmett@gmail.com>
