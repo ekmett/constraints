8728671e686c6321f0e0a090605512081dc5a53d
Edward Kmett <ekmett@gmail.com>
2015-03-25 01:40:03 -0400

    lifting a lot more stuff

--- Data/Constraint/Lifting.hs
+++ Data/Constraint/Lifting.hs
@@ -13,9 +13,12 @@ module Data.Constraint.Lifting
 
 import Control.Applicative
 import Control.DeepSeq
+import Control.Monad
+import Control.Monad.Fix
 import Data.Binary
 import Data.Constraint
 import Data.Foldable
+import Data.Functor.Classes
 import Data.Functor.Compose as Functor
 import Data.Functor.Product as Functor
 import Data.Functor.Sum as Functor
@@ -72,10 +75,47 @@ instance NFData a => Lifting NFData ((,) a)
 instance Monoid a => Lifting Monoid ((,) a)
 
 instance Functor f => Lifting Functor (Compose f)
+instance Foldable f => Lifting Foldable (Compose f)
+instance Traversable f => Lifting Traversable (Compose f)
 instance Applicative f => Lifting Applicative (Compose f)
+instance Alternative f => Lifting Alternative (Compose f) where lifting = Sub Dict -- overconstrained
+instance (Functor f, Show1 f) => Lifting Show1 (Compose f) where lifting = Sub Dict
+instance (Functor f, Eq1 f) => Lifting Eq1 (Compose f) where lifting = Sub Dict
+instance (Functor f, Ord1 f) => Lifting Ord1 (Compose f) where lifting = Sub Dict
+instance (Functor f, Read1 f) => Lifting Read1 (Compose f) where lifting = Sub Dict
+instance (Functor f, Eq1 f, Eq1 g) => Lifting Eq (Compose f g) where lifting = Sub Dict
+instance (Functor f, Ord1 f, Ord1 g) => Lifting Ord (Compose f g) where lifting = Sub Dict
+instance (Functor f, Read1 f, Read1 g) => Lifting Read (Compose f g) where lifting = Sub Dict
+instance (Functor f, Show1 f, Show1 g) => Lifting Show (Compose f g) where lifting = Sub Dict
 
 instance Functor f => Lifting Functor (Functor.Product f)
+instance Foldable f => Lifting Foldable (Functor.Product f)
+instance Traversable f => Lifting Traversable (Functor.Product f)
+instance Applicative f => Lifting Applicative (Functor.Product f)
+instance Alternative f => Lifting Alternative (Functor.Product f)
+instance Monad f => Lifting Monad (Functor.Product f)
+instance MonadFix f => Lifting MonadFix (Functor.Product f)
+instance MonadPlus f => Lifting MonadPlus (Functor.Product f)
+instance Show1 f => Lifting Show1 (Functor.Product f)
+instance Eq1 f => Lifting Eq1 (Functor.Product f)
+instance Ord1 f => Lifting Ord1 (Functor.Product f)
+instance Read1 f => Lifting Read1 (Functor.Product f)
+instance (Eq1 f, Eq1 g) => Lifting Eq (Functor.Product f g) where lifting = Sub Dict
+instance (Ord1 f, Ord1 g) => Lifting Ord (Functor.Product f g) where lifting = Sub Dict
+instance (Read1 f, Read1 g) => Lifting Read (Functor.Product f g) where lifting = Sub Dict
+instance (Show1 f, Show1 g) => Lifting Show (Functor.Product f g) where lifting = Sub Dict
+
 instance Functor f => Lifting Functor (Functor.Sum f)
+instance Foldable f => Lifting Foldable (Functor.Sum f)
+instance Traversable f => Lifting Traversable (Functor.Sum f)
+instance Show1 f => Lifting Show1 (Functor.Sum f)
+instance Eq1 f => Lifting Eq1 (Functor.Sum f)
+instance Ord1 f => Lifting Ord1 (Functor.Sum f)
+instance Read1 f => Lifting Read1 (Functor.Sum f)
+instance (Eq1 f, Eq1 g) => Lifting Eq (Functor.Sum f g) where lifting = Sub Dict
+instance (Ord1 f, Ord1 g) => Lifting Ord (Functor.Sum f g) where lifting = Sub Dict
+instance (Read1 f, Read1 g) => Lifting Read (Functor.Sum f g) where lifting = Sub Dict
+instance (Show1 f, Show1 g) => Lifting Show (Functor.Sum f g) where lifting = Sub Dict
 
 class Lifting2 p f where
   lifting2 :: (p a, p b) :- p (f a b)
@@ -101,11 +141,25 @@ instance Lifting2 Functor Compose where lifting2 = Sub Dict
 instance Lifting2 Foldable Compose where lifting2 = Sub Dict
 instance Lifting2 Traversable Compose where lifting2 = Sub Dict
 instance Lifting2 Applicative Compose where lifting2 = Sub Dict
+instance Lifting2 Alternative Compose where lifting2 = Sub Dict -- overconstrained
 
 instance Lifting2 Functor Functor.Product where lifting2 = Sub Dict
 instance Lifting2 Foldable Functor.Product where lifting2 = Sub Dict
 instance Lifting2 Traversable Functor.Product where lifting2 = Sub Dict
+instance Lifting2 Applicative Functor.Product where lifting2 = Sub Dict
+instance Lifting2 Alternative Functor.Product where lifting2 = Sub Dict
+instance Lifting2 Monad Functor.Product where lifting2 = Sub Dict
+instance Lifting2 MonadPlus Functor.Product where lifting2 = Sub Dict
+instance Lifting2 MonadFix Functor.Product where lifting2 = Sub Dict
+instance Lifting2 Show1 Functor.Product where lifting2 = Sub Dict
+instance Lifting2 Eq1 Functor.Product where lifting2 = Sub Dict
+instance Lifting2 Ord1 Functor.Product where lifting2 = Sub Dict
+instance Lifting2 Read1 Functor.Product where lifting2 = Sub Dict
 
 instance Lifting2 Functor Functor.Sum where lifting2 = Sub Dict
 instance Lifting2 Foldable Functor.Sum where lifting2 = Sub Dict
 instance Lifting2 Traversable Functor.Sum where lifting2 = Sub Dict
+instance Lifting2 Show1 Functor.Sum where lifting2 = Sub Dict
+instance Lifting2 Eq1 Functor.Sum where lifting2 = Sub Dict
+instance Lifting2 Ord1 Functor.Sum where lifting2 = Sub Dict
+instance Lifting2 Read1 Functor.Sum where lifting2 = Sub Dict
