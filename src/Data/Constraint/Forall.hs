522e9da30410898895f7d13194ea8ac50910a01c
Edward Kmett <ekmett@gmail.com>
2015-08-10 14:23:16 -0400

    Completely rewite Forall to avoid using unexported skolems to avoid the subversions in #10 and #11. This loses us some functionality, but restores safety.

--- src/Data/Constraint/Forall.hs
+++ src/Data/Constraint/Forall.hs
@@ -6,10 +6,10 @@
 {-# LANGUAGE EmptyDataDecls #-}
 {-# LANGUAGE KindSignatures #-}
 {-# LANGUAGE TypeOperators #-}
-{-# LANGUAGE Unsafe #-}
-#if __GLASGOW_HASKELL__ >= 707
-{-# LANGUAGE RoleAnnotations #-}
-#endif
+{-# LANGUAGE Trustworthy #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE PolyKinds #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
 -----------------------------------------------------------------------------
 -- |
 -- Module      :  Data.Constraint.Forall
@@ -21,51 +21,34 @@
 -- Portability :  non-portable
 --
 -- This module uses a trick to provide quantification over constraints.
---
--- Sadly this approach has been subverted.
 ----------------------------------------------------------------------------
 
 module Data.Constraint.Forall
-  ( Forall, inst
-  , ForallF, instF
-  , Forall1, inst1
-  , ForallT, instT
+  ( Forall(..), inst, forall
+  , ForallT(..), instT, forallT
   ) where
 
 import Data.Constraint
-import Data.Constraint.Unsafe
+import Unsafe.Coerce
 
--- skolem variables, do not export!
-data A
-data B
--- | A quantified constraint
-type Forall (p :: * -> Constraint) = (p A, p B)
+class Forall p where
+  instantiate :: Dict (p a)
 
-type ForallF (p :: * -> Constraint) (f :: * -> *) = (p (f A), p (f B))
+inst :: Forall p :- p a
+inst = Sub instantiate
 
-data F a
-data M a
+newtype Magic p r = Magic (Forall p => Dict (Forall p))
 
-type Forall1 (p :: (* -> *) -> Constraint) = (p F, p M)
+forall :: (forall a. Dict (p a)) -> Dict (Forall p)
+forall p = unsafeCoerce (Magic Dict) p
 
-type ForallT (p :: * -> Constraint) (t :: (* -> *) -> * -> *) = (p (t F A), p (t M B))
+class ForallT p t where
+  instantiateT :: Dict (p (t f a))
 
-#if __GLASGOW_HASKELL__ >= 707
-type role F nominal
-type role M nominal
-#endif
+instT :: ForallT p t :- p (t f a)
+instT = Sub instantiateT
 
--- | instantiate a quantified constraint on kind @*@
-inst :: forall p a. Forall p :- p a
-inst = trans (unsafeCoerceConstraint :: p A :- p a) weaken1
+newtype MagicT p t r = MagicT (ForallT p t => Dict (ForallT p t))
 
-instF :: forall p f a. ForallF p f :- p (f a)
-instF = trans (unsafeCoerceConstraint :: p (f A) :- p (f a)) weaken1
-
--- | instantiate a quantified constraint on kind @* -> *@
-inst1 :: forall (p :: (* -> *) -> Constraint) (f :: * -> *). Forall1 p :- p f
-inst1 = trans (unsafeCoerceConstraint :: p F :- p f) weaken1
-
-instT :: forall (p :: * -> Constraint) (t :: (* -> *) -> * -> *) (f :: * -> *) a. ForallT p t :- p (t f a)
-instT = trans (unsafeCoerceConstraint :: p (t F A) :- p (t f a)) weaken1
-
+forallT :: (forall f a. Dict (p (t f a))) -> Dict (ForallT p t)
+forallT p = unsafeCoerce (MagicT Dict) p
