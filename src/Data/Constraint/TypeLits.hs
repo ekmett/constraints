9ac96adf66ad7a9400f4b8bdbcae43c9d8bb9398
Edward Kmett <ekmett@gmail.com>
2016-07-28 16:39:32 -0400

    embace explicit type application for Data.Constraint.TypeLits as we can't build an empty closed type family in 7.10 anyways

--- src/Data/Constraint/TypeLits.hs
+++ src/Data/Constraint/TypeLits.hs
@@ -4,7 +4,9 @@
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE AllowAmbiguousTypes #-}
 {-# LANGUAGE Trustworthy #-}
 module Data.Constraint.TypeLits
   ( Min
@@ -27,6 +29,7 @@ import GHC.TypeLits
 import Unsafe.Coerce
 
 type family Min (n :: Nat) (m :: Nat) :: Nat where
+
 type family Max (n :: Nat) (m :: Nat) :: Nat where
 
 newtype Magic n r = Magic (KnownNat n => r)
@@ -37,8 +40,8 @@ magic f = Sub $ unsafeCoerce (Magic id) (natVal (Proxy :: Proxy n) `f` natVal (P
 axiom :: forall a b. Dict (a ~ b)
 axiom = unsafeCoerce (Dict :: Dict (a ~ a))
 
-axiomLe :: forall a b p1 p2. p1 a -> p2 b -> Dict (a <= b)
-axiomLe _ _ = axiom
+axiomLe :: forall a b. Dict (a <= b)
+axiomLe = axiom
 
 eqLe :: (a ~ b) :- (a <= b)
 eqLe = Sub Dict
@@ -58,103 +61,103 @@ timesNat = magic (*)
 powNat :: forall n m. (KnownNat n, KnownNat m) :- KnownNat (n ^ m)
 powNat = magic (^)
 
-plusZero :: forall n p1. p1 n -> Dict ((n + 0) ~ n)
-plusZero _ = axiom
+plusZero :: forall n. Dict ((n + 0) ~ n)
+plusZero = axiom
 
-timesZero :: forall n p1. p1 n -> Dict ((n * 0) ~ 0)
-timesZero _ = axiom
+timesZero :: forall n. Dict ((n * 0) ~ 0)
+timesZero = axiom
 
-timesOne :: forall n p1. p1 n -> Dict ((n * 1) ~ n)
-timesOne _ = axiom
+timesOne :: forall n. Dict ((n * 1) ~ n)
+timesOne = axiom
 
-minZero :: forall n p1. p1 n -> Dict (Min n 0 ~ 0)
-minZero _ = axiom
+minZero :: forall n. Dict (Min n 0 ~ 0)
+minZero = axiom
 
-maxZero :: forall n p1. p1 n -> Dict (Max n 0 ~ n)
-maxZero _ = axiom
+maxZero :: forall n. Dict (Max n 0 ~ n)
+maxZero = axiom
 
-powZero :: forall n p1. p1 n -> Dict ((n ^ 0) ~ 1)
-powZero _ = axiom
+powZero :: forall n. Dict ((n ^ 0) ~ 1)
+powZero = axiom
 
-powOne :: forall n p1. p1 n -> Dict ((n ^ 1) ~ n)
-powOne _ = axiom
+powOne :: forall n. Dict ((n ^ 1) ~ n)
+powOne = axiom
 
-plusCommutes :: forall n m p1 p2. p1 n -> p2 m -> Dict ((m + n) ~ (n + m))
-plusCommutes _ _ = axiom
+plusCommutes :: forall n m. Dict ((m + n) ~ (n + m))
+plusCommutes = axiom
 
-timesCommutes :: forall n m p1 p2. p1 n -> p2 m -> Dict ((m * n) ~ (n * m))
-timesCommutes _ _ = axiom
+timesCommutes :: forall n m. Dict ((m * n) ~ (n * m))
+timesCommutes = axiom
 
-minCommutes :: forall n m p1 p2. p1 n -> p2 m -> Dict (Min m n ~ Min n m)
-minCommutes _ _ = axiom
+minCommutes :: forall n m. Dict (Min m n ~ Min n m)
+minCommutes = axiom
 
-maxCommutes :: forall n m p1 p2. p1 n -> p2 m -> Dict (Min m n ~ Min n m)
-maxCommutes _ _ = axiom
+maxCommutes :: forall n m. Dict (Min m n ~ Min n m)
+maxCommutes = axiom
 
-plusAssociates :: forall n m o p1 p2 p3. p1 n -> p2 m -> p3 o -> Dict (((m + n) + o) ~ (m + (n + o)))
-plusAssociates _ _ _ = axiom
+plusAssociates :: forall n m o. Dict (((m + n) + o) ~ (m + (n + o)))
+plusAssociates = axiom
 
-timesAssociates :: forall n m o p1 p2 p3. p1 n -> p2 m -> p3 o -> Dict (((m * n) * o) ~ (m * (n * o)))
-timesAssociates _ _ _ = axiom
+timesAssociates :: forall n m o. Dict (((m * n) * o) ~ (m * (n * o)))
+timesAssociates = axiom
 
-minAssociates :: forall n m o p1 p2 p3. p1 n -> p2 m -> p3 o -> Dict (Min (Min m n) o ~ Min m (Min n o))
-minAssociates _ _ _ = axiom
+minAssociates :: forall n m o. Dict (Min (Min m n) o ~ Min m (Min n o))
+minAssociates = axiom
 
-maxAssociates :: forall n m o p1 p2 p3. p1 n -> p2 m -> p3 o -> Dict (Max (Max m n) o ~ Max m (Max n o))
-maxAssociates _ _ _ = axiom
+maxAssociates :: forall n m o. Dict (Max (Max m n) o ~ Max m (Max n o))
+maxAssociates = axiom
 
-minIsIdempotent :: forall n p1. p1 n -> Dict (Min n n ~ n)
-minIsIdempotent _ = axiom
+minIsIdempotent :: forall n. Dict (Min n n ~ n)
+minIsIdempotent = axiom
 
-maxIsIdempotent :: forall n p1. p1 n -> Dict (Max n n ~ n)
-maxIsIdempotent _ = axiom
+maxIsIdempotent :: forall n. Dict (Max n n ~ n)
+maxIsIdempotent = axiom
 
-minDistributesOverPlus :: forall n m o p1 p2 p3. p1 n -> p2 m -> p3 o -> Dict ((n + Min m o) ~ Min (n + m) (n + o))
-minDistributesOverPlus _ _ _ = axiom
+minDistributesOverPlus :: forall n m o. Dict ((n + Min m o) ~ Min (n + m) (n + o))
+minDistributesOverPlus = axiom
 
-minDistributesOverTimes :: forall n m o p1 p2 p3. p1 n -> p2 m -> p3 o -> Dict ((n * Min m o) ~ Min (n * m) (n * o))
-minDistributesOverTimes _ _ _ = axiom
+minDistributesOverTimes :: forall n m o. Dict ((n * Min m o) ~ Min (n * m) (n * o))
+minDistributesOverTimes = axiom
 
-minDistributesOverPow1 :: forall n m o p1 p2 p3. p1 n -> p2 m -> p3 o -> Dict ((Min n m ^ o) ~ Min (n ^ o) (m ^ o))
-minDistributesOverPow1 _ _ _ = axiom
+minDistributesOverPow1 :: forall n m o. Dict ((Min n m ^ o) ~ Min (n ^ o) (m ^ o))
+minDistributesOverPow1 = axiom
 
-minDistributesOverPow2 :: forall n m o p1 p2 p3. p1 n -> p2 m -> p3 o -> Dict ((n ^ Min m o) ~ Min (n ^ m) (n ^ o))
-minDistributesOverPow2 _ _ _ = axiom
+minDistributesOverPow2 :: forall n m o. Dict ((n ^ Min m o) ~ Min (n ^ m) (n ^ o))
+minDistributesOverPow2 = axiom
 
-minDistributesOverMax :: forall n m o p1 p2 p3. p1 n -> p2 m -> p3 o -> Dict (Max n (Min m o) ~ Min (Max n m) (Max n o))
-minDistributesOverMax _ _ _ = axiom
+minDistributesOverMax :: forall n m o. Dict (Max n (Min m o) ~ Min (Max n m) (Max n o))
+minDistributesOverMax = axiom
 
-maxDistributesOverPlus :: forall n m o p1 p2 p3. p1 n -> p2 m -> p3 o -> Dict ((n + Max m o) ~ Max (n + m) (n + o))
-maxDistributesOverPlus _ _ _ = axiom
+maxDistributesOverPlus :: forall n m o. Dict ((n + Max m o) ~ Max (n + m) (n + o))
+maxDistributesOverPlus = axiom
 
-maxDistributesOverTimes :: forall n m o p1 p2 p3. p1 n -> p2 m -> p3 o -> Dict ((n * Max m o) ~ Max (n * m) (n * o))
-maxDistributesOverTimes _ _ _ = axiom
+maxDistributesOverTimes :: forall n m o. Dict ((n * Max m o) ~ Max (n * m) (n * o))
+maxDistributesOverTimes = axiom
 
-maxDistributesOverPow1 :: forall n m o p1 p2 p3. p1 n -> p2 m -> p3 o -> Dict ((Max n m ^ o) ~ Max (n ^ o) (m ^ o))
-maxDistributesOverPow1 _ _ _ = axiom
+maxDistributesOverPow1 :: forall n m o. Dict ((Max n m ^ o) ~ Max (n ^ o) (m ^ o))
+maxDistributesOverPow1 = axiom
 
-maxDistributesOverPow2 :: forall n m o p1 p2 p3. p1 n -> p2 m -> p3 o -> Dict ((n ^ Max m o) ~ Max (n ^ m) (n ^ o))
-maxDistributesOverPow2 _ _ _ = axiom
+maxDistributesOverPow2 :: forall n m o. Dict ((n ^ Max m o) ~ Max (n ^ m) (n ^ o))
+maxDistributesOverPow2 = axiom
 
-maxDistributesOverMin :: forall n m o p1 p2 p3. p1 n -> p2 m -> p3 o -> Dict (Min n (Max m o) ~ Max (Min n m) (Min n o))
-maxDistributesOverMin _ _ _ = axiom
+maxDistributesOverMin :: forall n m o. Dict (Min n (Max m o) ~ Max (Min n m) (Min n o))
+maxDistributesOverMin = axiom
 
-plusDistributesOverTimes :: forall n m o p1 p2 p3. p1 n -> p2 m -> p3 o -> Dict ((n * (m + o)) ~ (n * m + n * o))
-plusDistributesOverTimes _ _ _ = axiom
+plusDistributesOverTimes :: forall n m o. Dict ((n * (m + o)) ~ (n * m + n * o))
+plusDistributesOverTimes = axiom
 
-timesDistributesOverPow  :: forall n m o p1 p2 p3. p1 n -> p2 m -> p3 o -> Dict ((n ^ (m + o)) ~ (n ^ m * n ^ o))
-timesDistributesOverPow _ _ _ = axiom
+timesDistributesOverPow  :: forall n m o. Dict ((n ^ (m + o)) ~ (n ^ m * n ^ o))
+timesDistributesOverPow = axiom
 
-plusIsCancellative :: forall n m o p1 p2 p3. p1 n -> p2 m -> p3 o -> ((n + m) ~ (n + o)) :- (m ~ o)
-plusIsCancellative _ _ _ = Sub axiom
+plusIsCancellative :: forall n m o. ((n + m) ~ (n + o)) :- (m ~ o)
+plusIsCancellative = Sub axiom
 
 -- (<=) is an internal category in the category of constraints.
 
-leId :: forall a p1. p1 a -> Dict (a <= a)
-leId _ = Dict
+leId :: forall a. Dict (a <= a)
+leId = Dict
 
-leEq :: forall a b p1 p2. p1 a -> p2 b -> (a <= b, b <= a) :- (a ~ b)
-leEq _ _ = Sub axiom
+leEq :: forall a b. (a <= b, b <= a) :- (a ~ b)
+leEq = Sub axiom
 
-leTrans :: forall a b c p1 p2 p3. p1 a -> p2 b -> p3 c -> (b <= c, a <= b) :- (a <= c)
-leTrans _ _ _ = Sub (axiomLe (Proxy :: Proxy a) (Proxy :: Proxy c))
+leTrans :: forall a b c. (b <= c, a <= b) :- (a <= c)
+leTrans = Sub (axiomLe @a @c)
