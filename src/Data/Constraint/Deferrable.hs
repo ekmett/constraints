1ae21e22907609ffdc05c3e92b2ef1236e8f34f7
David Feuer <David.Feuer@gmail.com>
2018-05-10 00:20:09 -0400

    Add some Deferrable extras
    * More constraint tuple instances, because we can.
    * A nice default definition for never-satisfiable constraints,
      and a stupidly boring always-satisfiable one for symmetry.

--- src/Data/Constraint/Deferrable.hs
+++ src/Data/Constraint/Deferrable.hs
@@ -7,6 +7,9 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE DeriveDataTypeable #-}
 {-# LANGUAGE PolyKinds #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE UndecidableInstances #-}
 
 #if __GLASGOW_HASKELL__ >= 800
 {-# LANGUAGE TypeApplications #-}
@@ -38,6 +41,8 @@ module Data.Constraint.Deferrable
   , (:~~:)(HRefl)
 #endif
   , (:~:)(Refl)
+  , deferEitherSuccess
+  , deferEitherFailure
   ) where
 
 import Control.Exception
@@ -59,11 +64,27 @@ data UnsatisfiedConstraint = UnsatisfiedConstraint String
   deriving (Typeable, Show)
 
 instance Exception UnsatisfiedConstraint
+#if MIN_VERSION_base(4,8,0)
+  where
+    displayException (UnsatisfiedConstraint str) = str
+#endif
 
 -- | Allow an attempt at resolution of a constraint at a later time
 class Deferrable p where
   -- | Resolve a 'Deferrable' constraint with observable failure.
   deferEither :: proxy p -> (p => r) -> Either String r
+
+-- | A good definition of 'deferEither' for a constraint that is never
+-- satisfiable. The error message will indicate what constraint could
+-- not be satisfied.
+deferEitherFailure :: Typeable p => proxy p -> (p => r) -> Either String r
+deferEitherFailure (_ :: proxy p) _ =
+  Left $ "Could not satisfy deferred constraint `" ++ showTypeRep (Proxy :: Proxy p) ++ "’"
+
+-- | A good definition of 'deferEither' for a constraint that is always
+-- satisfiable.
+deferEitherSuccess :: p => proxy p -> (p => r) -> Either String r
+deferEitherSuccess _ r = Right r
 
 -- | Defer a constraint for later resolution in a context where we want to upgrade failure into an error
 defer :: forall p r proxy. Deferrable p => proxy p -> (p => r) -> r
@@ -116,7 +137,7 @@ instance (Typeable a, Typeable b) => Deferrable (a ~ b) where
   deferEither _ r = case cast (Refl :: a :~: a) :: Maybe (a :~: b) of
     Just Refl -> Right r
     Nothing   -> Left $
-      "deferred type equality: type mismatch between `" ++ showTypeRep (Proxy :: Proxy a) ++ "’ and `"  ++ showTypeRep (Proxy :: Proxy b) ++ "'"
+      "Deferred type equality: type mismatch between `" ++ showTypeRep (Proxy :: Proxy a) ++ "’ and `"  ++ showTypeRep (Proxy :: Proxy b) ++ "'"
 
 #if __GLASGOW_HASKELL__ >= 800
 -- | Deferrable heterogenous equality constraints.
@@ -126,11 +147,103 @@ instance (Typeable i, Typeable j, Typeable (a :: i), Typeable (b :: j)) => Defer
   deferEither _ r = case cast (HRefl :: a :~~: a) :: Maybe (a :~~: b) of
     Just HRefl -> Right r
     Nothing   -> Left $
-      "deferred type equality: type mismatch between `" ++ showTypeRep (Proxy :: Proxy a) ++ "’ and `"  ++ showTypeRep (Proxy :: Proxy b) ++ "'"
+      "Deferred type equality: type mismatch between `" ++ showTypeRep (Proxy :: Proxy a) ++ "’ and `"  ++ showTypeRep (Proxy :: Proxy b) ++ "'"
 #endif
 
-instance (Deferrable a, Deferrable b) => Deferrable (a, b) where
-  deferEither _ r = join $ deferEither (Proxy :: Proxy a) $ deferEither (Proxy :: Proxy b) r
+instance ( Deferrable a
+         , Deferrable b
+         ) => Deferrable (a, b) where
+  deferEither _ = deferAmong (Proxy :: Proxy '[a,b])
 
-instance (Deferrable a, Deferrable b, Deferrable c) => Deferrable (a, b, c) where
-  deferEither _ r = join $ deferEither (Proxy :: Proxy a) $ join $ deferEither (Proxy :: Proxy b) $ deferEither (Proxy :: Proxy c) r
+instance ( Deferrable a
+         , Deferrable b
+         , Deferrable c
+         ) => Deferrable (a, b, c) where
+  deferEither _ = deferAmong (Proxy :: Proxy '[a,b,c])
+
+instance ( Deferrable a
+         , Deferrable b
+         , Deferrable c
+         , Deferrable d
+         ) => Deferrable (a,b,c,d) where
+  deferEither _ = deferAmong (Proxy :: Proxy '[a,b,c,d])
+
+instance ( Deferrable a
+         , Deferrable b
+         , Deferrable c
+         , Deferrable d
+         , Deferrable e
+         ) => Deferrable (a,b,c,d,e) where
+  deferEither _ = deferAmong (Proxy :: Proxy '[a,b,c,d,e])
+
+instance ( Deferrable a
+         , Deferrable b
+         , Deferrable c
+         , Deferrable d
+         , Deferrable e
+         , Deferrable f
+         ) => Deferrable (a,b,c,d,e,f) where
+  deferEither _ = deferAmong (Proxy :: Proxy '[a,b,c,d,e,f])
+
+instance ( Deferrable a
+         , Deferrable b
+         , Deferrable c
+         , Deferrable d
+         , Deferrable e
+         , Deferrable f
+         , Deferrable g
+         ) => Deferrable (a,b,c,d,e,f,g) where
+  deferEither _ = deferAmong (Proxy :: Proxy '[a,b,c,d,e,f,g])
+
+instance ( Deferrable a
+         , Deferrable b
+         , Deferrable c
+         , Deferrable d
+         , Deferrable e
+         , Deferrable f
+         , Deferrable g
+         , Deferrable h
+         ) => Deferrable (a,b,c,d,e,f,g,h) where
+  deferEither _ = deferAmong (Proxy :: Proxy '[a,b,c,d,e,f,g,h])
+
+instance ( Deferrable a
+         , Deferrable b
+         , Deferrable c
+         , Deferrable d
+         , Deferrable e
+         , Deferrable f
+         , Deferrable g
+         , Deferrable h
+         , Deferrable i
+         ) => Deferrable (a,b,c,d,e,f,g,h,i) where
+  deferEither _ = deferAmong (Proxy :: Proxy '[a,b,c,d,e,f,g,h,i])
+
+instance ( Deferrable a
+         , Deferrable b
+         , Deferrable c
+         , Deferrable d
+         , Deferrable e
+         , Deferrable f
+         , Deferrable g
+         , Deferrable h
+         , Deferrable i
+         , Deferrable j
+         ) => Deferrable (a,b,c,d,e,f,g,h,i,j) where
+  deferEither _ = deferAmong (Proxy :: Proxy '[a,b,c,d,e,f,g,h,i,j])
+
+-- This machinery serves only to reduce the amount of source code we have
+-- to write to support large tuples.
+type family AllC (cs :: [Constraint]) :: Constraint where
+  AllC '[] = ()
+  AllC (c ': cs) = (c, AllC cs)
+
+type family AllD (cs :: [Constraint]) :: Constraint where
+  AllD '[] = ()
+  AllD (c ': cs) = (Deferrable c, AllD cs)
+
+class DeferrableList (cs :: [Constraint]) where
+  deferAmong :: proxy cs -> (AllC cs => r) -> Either String r
+instance DeferrableList '[] where
+  deferAmong _ r = Right r
+instance (Deferrable c, DeferrableList cs) => DeferrableList (c ': cs) where
+  deferAmong _ r = join $ deferEither (Proxy :: Proxy c) $ deferAmong (Proxy :: Proxy cs) r
